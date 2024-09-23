797d30a2eede622a610a375c183734b3443c07f7
RohanZade <111170edu@gmail.com>
2024-09-23 17:09:38 +0530

    remove wringting using pandas and using boto3

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,8 @@
 import random
 import pandas as pd
+from io import StringIO # python3; python2: BytesIO 
+import boto3
+
 
 def generate_and_store_transactions_data_to_s3(event, context):
     print(">>>>>>>",event)
@@ -20,10 +23,16 @@ def generate_and_store_transactions_data_to_s3(event, context):
     year = event_str[0]
     month = event_str[1]
     day = event_str[2]
-    df.to_csv(f"s3://m3c1-assignment/transactions_data/year={year}/month={month}/day={day}/transactions_{event['date']}.csv", index=False)
+    # df.to_csv(f"s3://m3c1-assignment/transactions_data/year={year}/month={month}/day={day}/transactions_{event['date']}.csv", index=False)
+    
+    bucket = 'm3c1-assignment' # already created on S3
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object(bucket, f'transactions_data/year={year}/month={month}/day={day}/transactions_{event["date"]}.csv').put(Body=csv_buffer.getvalue())
 
 
-# if __name__ == "__main__":
-#     generate_and_store_transactions_data_to_s3({"date":"2024-09-23"},2)
+if __name__ == "__main__":
+    generate_and_store_transactions_data_to_s3({"date":"2024-09-23"},2)
 
     
